d59a71927b1aba1e4de97fe742ff11f7ebddaff6
santhoskumar1997 <kumarstar2697@gmail.com>
2021-06-14 15:14:53 +0530

    added

---
+++ src/Hellos.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hellos {
+public static void main(String[] args) {
+	
+	Scanner tt=new Scanner(System.in);
+	int k = tt.nextInt();
+	System.out.println("enter the number"+k);
+}
+	
+}
+	
+	
